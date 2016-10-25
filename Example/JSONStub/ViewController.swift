99cb91559448ea3b9fc7190b289a14b909d7f2c8
Rafael da Silva Ferreira <rafael@swift-yah.io>
2016-10-25 16:27:23 -0200

    Update all Pod to Swift 3 and Xcode 8
    We update all workspace to use Swift 3 and Xcode 8.

--- Example/JSONStub/ViewController.swift
+++ Example/JSONStub/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  Controller.swift
 //  JSONStub
 //
 //  Created by Rafael da Silva Ferreira on 10/25/2016.
@@ -8,17 +8,5 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+final class Controller: UIViewController {
 }
-
